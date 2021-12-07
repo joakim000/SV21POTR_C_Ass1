3119bc9a0399f31e852aa16f8ba22d2e5e416bc7
Joakim <joakim.odermalm@gmail.com>
2021-12-07 20:38:41 +0100

    Example tests

--- test/test_buffer.c
+++ test/test_buffer.c
@@ -1,7 +1,8 @@
 #include "unity.h"
 #include "buffer.h"
 
-uint8_t buf[8] = {0};
+#define BUFSIZE sizeof(uint64_t)
+uint8_t buf[BUFSIZE] = {0};
 
 
 void setUp(void) {
@@ -12,6 +13,8 @@ void tearDown(void) {}
 
 void Test_buffer_insert_exanple() {
     buffer_insert(buf, 12, 6, 24);
+    uint8_t expected[BUFSIZE] = {0, 128, 1, 0, 0, 0, 0, 0};
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected, buf, BUFSIZE);
 }
 
 void Test_buffer_extract_example() {
@@ -37,10 +40,10 @@ int main(void)
     RUN_TEST(Test_buffer_insert_exanple);
     RUN_TEST(Test_buffer_extract_example);
 
-    RUN_TEST(Test_buffer_insert_64bit);
-    RUN_TEST(Test_buffer_extract_64bit);
+    // RUN_TEST(Test_buffer_insert_64bit);
+    // RUN_TEST(Test_buffer_extract_64bit);
 
-    RUN_TEST(Test_buffer_insert_wrongLen);
+    // RUN_TEST(Test_buffer_insert_wrongLen);
 
     return UNITY_END();
 }
